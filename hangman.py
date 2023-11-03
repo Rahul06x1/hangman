caa160be3ea3e65d1ed01abca935a6335fcc7e37
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-03 15:53:29 +0530

    Implement function get_random_word [ Green: Lowercase letters only ]

---
+++ hangman.py
@@ -0,0 +1,13 @@
+import random
+
+def get_random_word(wordlist="/usr/share/dict/words"):
+    good_words = []
+    with open(wordlist) as f:
+        words = [x.strip() for x in f]
+        for word in words:
+            if not word.islower(): # if it is a proper noun
+                continue
+            good_words.append(word)
+
+        secret_word = random.choice(good_words)
+        return secret_word
